2af6eb842ab1b176904ad10defb453e83a0f8297
sainathGit <sainathc6@gmail.com>
2017-10-31 16:29:54 +0530

    2nd commit issafe not working

--- main.py
+++ main.py
@@ -15,41 +15,56 @@ class_room = [[-1 for i in range(n)] for j in range(m)]
 
 def is_safe(place, student):
 
+    print(place)
     row, col = place
 
     for i in range(m):
         for j in range(n):
+            
+            is_not_empty = class_room[row][col] != -1
             in_fight_position = (i,j) != place and j != col and i != row and i-j == row - col and i+j == row + col
-            if (in_fight_position and class_room[row][col] not in friends[student]):
+            if (is_not_empty and in_fight_position and class_room[row][col] not in friends[student]):
+                print("False")
                 return False
     return True
 
 
 def arrange_backtrack(place):
+        print(place)
+        row = place // n
+        col = place % n
 
-    for i in range(m*n):
-        row = i // n
-        col = i % m
-
-        if placed[i] != 1 and is_safe((row,col),i):
-
-           # print(class_room)
-
-            if place == m*n -1:
-                for i in range(m):
-                    print(class_room[i])
-                return
-
-
-            class_room[row][col] = i
-            arrange_backtrack(place + 1)
-
-    class_room[row][col] = -1
+        for i in range(m*n):
+            if placed[i] != 1 and is_safe((row,col),i):
 
 
 
+                if place == m*n -1:
+                    class_room[row][col] = i
+                    for i in range(m):
+                      print(class_room[i])
+
+                    return True
+
+                class_room[row][col] = i
+                placed[i] = 1
+                print(placed)
+                print(class_room)
+
+
+                found = arrange_backtrack(place + 1)
+                if found :
+                    return True
+
+                placed[i] = 0
+
+
+        class_room[row][col] = -1
 
 
 
+
+
+arrange_backtrack(0)
 
 
